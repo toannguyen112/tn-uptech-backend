c1c1af2457b2e2c3c17f177437339981345f0ceb
toannguyen112 <nguyencongtoana1nhvt@gmail.com>
2023-04-23 14:16:53 +0700

    Update logs.route.ts

--- src/route/logs.route.ts
+++ src/route/logs.route.ts
@@ -2,7 +2,7 @@
 import { Router } from "express";
 import { Routes } from "../interface/routes.interface";
 import { LogController } from "../modules/controllers/log.controller";
-export class PostRoute implements Routes {
+export class LogsRoute implements Routes {
     public path = '/logs';
     public router = Router();
     public log = new LogController();
