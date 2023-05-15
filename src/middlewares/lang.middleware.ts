9888090522b2869a3508e2d09318cd20fc20b63b
toannguyen112 <nguyencongtoana1nhvt@gmail.com>
2023-05-16 00:35:28 +0700

    Better Code

--- src/middlewares/lang.middleware.ts
+++ src/middlewares/lang.middleware.ts
@@ -4,7 +4,15 @@ export interface LanguageRequest extends Request {
     lang: string
 }
 
-export const LangMiddleware = (req: LanguageRequest, res: Response, next: NextFunction) => {
+export const LangMiddleware = (req: any, res: Response, next: NextFunction) => {
+    const lang = req.acceptsLanguages('vi', 'en', 'ja');
+
+    if (lang) {
+        global.lang = lang;
+    }
+    else {
+        global.lang = 'vi';
+    }
 
     next();
 };
