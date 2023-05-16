dd9579d1afd1e04cf6c4bc153f4c0195d8ff861a
toannguyen112 <nguyencongtoana1nhvt@gmail.com>
2023-05-16 22:56:35 +0700

    Better Code

--- src/modules/controllers/log.controller.ts
+++ src/modules/controllers/log.controller.ts
@@ -2,7 +2,7 @@ import { NextFunction, Request, Response } from 'express';
 
 export class LogController {
 
-    public async index() {
+    public async index(req: Request, res: Response, next: NextFunction) {
         return res.status(200).send("ok");
     }
 
