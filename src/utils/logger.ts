f2606a6be1f50560ba6b791e05da55156ce0ed68
toannguyen112 <nguyencongtoana1nhvt@gmail.com>
2023-06-28 18:04:25 +0700

    code

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -2,10 +2,10 @@ import { existsSync, mkdirSync } from 'fs';
 import { join } from 'path';
 import winston from 'winston';
 import winstonDaily from 'winston-daily-rotate-file';
-import { LOG_DIR } from '../config';
+// import { LOG_DIR } from '../config';
+import 'dotenv/config'
 
-// logs dir
-const logDir: string = join(__dirname, LOG_DIR);
+const logDir: string = join(__dirname, process.env.LOG_DIR);
 
 if (!existsSync(logDir)) {
     mkdirSync(logDir);
