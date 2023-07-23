218c566b910a641984c8cdc2a43f6e7a296b275c
toannguyen112 <nguyencongtoana1nhvt@gmail.com>
2023-07-23 22:29:58 +0700

    update

--- src/modules/controllers/commont.controller.ts
+++ src/modules/controllers/commont.controller.ts
@@ -5,15 +5,6 @@ import Helper from '../../utils/Helper';
 export class CommontController extends BaseController {
 
     public async emptyDirSync(req: Request, res: Response, next: NextFunction) {
-
-        try {
-            await Helper.emptyDirSync();
-            res.status(200).send({ message: "clear images success" });
-
-        } catch (error) {
-            logger.error(error);
-            res.status(500).send(error.message);
-        }
     }
 
     public async index(req: Request, res: Response, next: NextFunction) {
